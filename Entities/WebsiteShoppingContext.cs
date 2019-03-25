22894d3a36a969af80690e068639a4ba0084f606
Thanh Hung <hungpt.se@gmail.com>
2019-03-26 03:34:39 +0700

    Deploy

--- Entities/WebsiteShoppingContext.cs
+++ Entities/WebsiteShoppingContext.cs
@@ -29,8 +29,8 @@ namespace ShopAPI.Entities
         {
            if (!optionsBuilder.IsConfigured)
            {
-               optionsBuilder.UseSqlServer("Data Source=HUNGPTSE63362;Initial Catalog=WebsiteShopping;User ID=sa;Password=sa;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
-               //optionsBuilder.UseSqlServer("Server=tcp:shopapi-hungpt.database.windows.net,1433;Initial Catalog=ShopAPI-HungPT_db;Persist Security Info=False;User ID=hungpt;Password=Hungbi1998;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+               //optionsBuilder.UseSqlServer("Data Source=HUNGPTSE63362;Initial Catalog=WebsiteShopping;User ID=sa;Password=sa;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+               optionsBuilder.UseSqlServer("Server=tcp:shopapi-hungpt.database.windows.net,1433;Initial Catalog=ShopAPI-HungPT_db;Persist Security Info=False;User ID=hungpt;Password=Hungbi1998;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
 
             }
         }
